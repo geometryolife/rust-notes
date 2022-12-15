79214b28609d1ad655dd74fefb092a8aecf486c1
Joe Chen <geometryolife@gmail.com>
2022-12-15 17:48:15 +0800

    use lazy_static library

---
+++ cpfrp/ch01/e7-use-lazy-static/src/main.rs
@@ -0,0 +1,23 @@
+use lazy_static::lazy_static;
+use std::collections::HashMap;
+
+lazy_static! {
+    static ref DICTIONARY: HashMap<u32, &'static str> = {
+        let mut m = HashMap::new();
+        m.insert(11, "foo");
+        m.insert(12, "bar");
+        println!("Initialized");
+        m
+    };
+}
+
+fn main() {
+    println!("Started");
+    println!("DICTIONARY contains {:?}", *DICTIONARY);
+    println!("DICTIONARY contains {:?}", *DICTIONARY);
+}
+
+// Started
+// Initialized
+// DICTIONARY contains {11: "foo", 12: "bar"}
+// DICTIONARY contains {11: "foo", 12: "bar"}
